a38524cb01a9ae4790418c1e382df7ccd9cca9cb
Prakarsh kashyap <126262042+prakarshhh@users.noreply.github.com>
2024-08-13 10:01:38 +0530

    Add files via upload

---
+++ exp.py
@@ -0,0 +1,109 @@
+
+import nltk
+nltk.download('gutenberg')
+from nltk.corpus import gutenberg
+import  pandas as pd
+
+
+data=gutenberg.raw('shakespeare-hamlet.txt')
+
+with open('hamlet.txt','w') as file:
+    file.write(data)
+    
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+
+with open('hamlet.txt','r') as file:
+    text=file.read().lower()
+
+
+tokenizer=Tokenizer()
+tokenizer.fit_on_texts([text])
+total_words=len(tokenizer.word_index)+1
+print(total_words)
+
+print(tokenizer.word_index)
+
+input_sequences=[]
+for line in text.split('\n'):
+    token_list=tokenizer.texts_to_sequences([line])[0]
+    for i in range(1,len(token_list)):
+        n_gram_sequence=token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+        
+print(input_sequences)
+
+max_sequence_len=max([len(x) for x in input_sequences])
+print(max_sequence_len)
+
+input_sequences=np.array(pad_sequences(input_sequences,maxlen=max_sequence_len,padding='pre'))
+print(input_sequences)
+
+import tensorflow as tf
+x,y=input_sequences[:,:-1],input_sequences[:,-1]
+
+print(x)
+print(y)
+
+y=tf.keras.utils.to_categorical(y,num_classes=total_words)
+print(y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+from tensorflow.keras.callbacks import EarlyStopping
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding,LSTM,Dense,Dropout,GRU
+
+## Define the model
+model=Sequential()
+model.add(Embedding(total_words,100,input_length=max_sequence_len-1))
+model.add(LSTM(150,return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(100))
+model.add(Dense(total_words,activation="softmax"))
+
+# #Compile the model
+model.compile(loss="categorical_crossentropy",optimizer='adam',metrics=['accuracy'])
+print(model.summary())
+
+model=Sequential()
+model.add(Embedding(total_words,100,input_length=max_sequence_len-1))
+model.add(GRU(150,return_sequences=True))
+model.add(Dropout(0.2))
+model.add(GRU(100))
+model.add(Dense(total_words,activation="softmax"))
+
+# #Compile the model
+model.compile(loss="categorical_crossentropy",optimizer='adam',metrics=['accuracy'])
+print(model.summary())
+history=model.fit(x_train,y_train,epochs=50,validation_data=(x_test,y_test),verbose=1,callbacks=[early_stopping])
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+input_text="To be or not to be"
+print(f"Input text:{input_text}")
+max_sequence_len=model.input_shape[1]+1
+next_word=predict_next_word(model,tokenizer,input_text,max_sequence_len)
+print(f"Next Word PRediction:{next_word}")
+
+model.save("next_word_lstm.h5")
+## Save the tokenizer
+import pickle
+with open('tokenizer.pickle','wb') as handle:
+    pickle.dump(tokenizer,handle,protocol=pickle.HIGHEST_PROTOCOL)
+    
+input_text="  Barn. Last night of all,When yond same"
+print(f"Input text:{input_text}")
+max_sequence_len=model.input_shape[1]+1
+next_word=predict_next_word(model,tokenizer,input_text,max_sequence_len)
+print(f"Next Word PRediction:{next_word}")
